e59ef3b1c42f324db29bdc61f55abe959a486311
samsagaz <joseluiszanotti@gmail.com>
2018-12-12 11:44:55 -0300

    working with auth

--- mercadolibre/pyme.py
+++ mercadolibre/pyme.py
@@ -1,6 +1,8 @@
 import os
 import requests
 import ssl
+import re
+from urllib.parse import urlencode
 from requests_toolbelt import SSLAdapter
 from configparser import SafeConfigParser
 
@@ -22,27 +24,45 @@ class PyMe(object):
         parser = SafeConfigParser()
         parser.read(os.path.dirname(os.path.abspath(__file__))+'/config.ini')
 
-        self.API_ROOT_URL = parser.get('config', 'api_root_url')
+        self.API_URL = parser.get('config', 'api_url')
         self.SDK_VERSION = parser.get('config', 'sdk_version')
         self.AUTH_URL = parser.get('config', 'auth_url')
-        self.OAUTH_URL = parser.get('config', 'oauth_url')
+        self.OAUTH_PATH = parser.get('config', 'oauth_path')
     
-    def auth_url(self,redirect_URI):
+    def auth_url(self, redirect_URI):
         params = {
             'client_id':self.client_id,
             'response_type':'code',
             'redirect_uri':redirect_URI}
         url = self.AUTH_URL  + '/authorization' + '?' + urlencode(params)
         return url
+    
+    def make_path(self, path, params=None):
+        params = params or {}
+        # Making Path and add a leading / if not exist
+        if not (re.search("^\/", path)):
+            path = "/" + path
+        path = self.API_URL + path
+        if params:
+            path = path + "?" + urlencode(params)
+        return path
 
     def authorize(self, code, redirect_URI):
-        params = { 'grant_type' : 'authorization_code', 'client_id' : self.client_id, 'client_secret' : self.client_secret, 'code' : code, 'redirect_uri' : redirect_URI}
-        headers = {'Accept': 'application/json', 'User-Agent':self.SDK_VERSION, 'Content-type':'application/json'}
-        uri = self.make_path(self.OAUTH_URL)
+        params = {
+            'grant_type' : 'authorization_code',
+            'client_id' : self.client_id,
+            'client_secret' : self.client_secret,
+            'code' : code,
+            'redirect_uri' : redirect_URI}
+        headers = {
+            'Accept': 'application/json',
+            'User-Agent':self.SDK_VERSION,
+            'Content-type':'application/json'}
+        uri = self.make_path(self.OAUTH_PATH)
 
         response = self._requests.post(uri, params=urlencode(params), headers=headers)
 
-        if response.status_code == requests.codes.ok:
+        if response.status_code == requests.codes.ok: #pylint: disable=E1101
             response_info = response.json()
             self.access_token = response_info['access_token']
             if 'refresh_token' in response_info:
@@ -57,4 +77,5 @@ class PyMe(object):
             response.raise_for_status()
 
     def display(self):
-        return {'id': self.id}
+        return {
+            'id': self.id}
